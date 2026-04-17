fb366ea18fd66f377e2daaf9cba8c70c46b74018
nasiman7862-cpu <nasiman7862@gmail.com>
2026-04-17 11:21:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if (task === "") {
+        alert("Enter something!");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = task;
+
+    li.onclick = function () {
+        this.remove();
+    };
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
